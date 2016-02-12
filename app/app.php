97b15acc2b55e57edbaf677aaafd1e3a96b15062
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-12 09:03:04 -0800

    Add Contact class with getters/setters

--- app/app.php
+++ app/app.php
@@ -1,9 +1,12 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/cd.php";
+    require_once __DIR__."/../src/book.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider, array (
       'twig.path' => __DIR__."/../views"
     ));
+
+
+    return $app;
  ?>
--- src/book.php
+++ src/book.php
@@ -4,6 +4,47 @@
       private $number;
       private $address;
 
-
+      function __construct($contact_name, $contact_number, $contact_address)
+      {
+        $this->name = $contact_name;
+        $this->number = $contact_number;
+        $this->address = $contact_address;
+      }
+      function setName($new_name)
+      {
+        $this->name = $new_name;
+      }
+      function setNumber($new_number)
+      {
+        $this->number = $new_number;
+      }
+      function setAddress($new_address)
+      {
+        $this->address = $new_address;
+      }
+      function getName()
+      {
+        return $this->name;
+      }
+      function getNumber()
+      {
+        return $this->number;
+      }
+      function getAddress()
+      {
+        return $this->address;
+      }
+      function save()
+      {
+        array_push($_SESSION['list_of_contacts'], $this);
+      }
+      static function getAll()
+      {
+        return $_SESSION['list_of_contacts'];
+      }
+      static function deleteAll()
+      {
+        $_SESSION['list_of_contacts'] = array();
+      }
     }
  ?>
